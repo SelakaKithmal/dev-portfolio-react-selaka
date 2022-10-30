59f0cd25b697387f2fc8679b71d79061992bf900
Selaka Nanayakkara <selaka.nanayakkara@cube.global>
2022-10-30 23:42:22 +0530

    added new configurations

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
 import { lightTheme, darkTheme } from './theme/themes';
 const firebaseConfig = {
-  apiKey: 'AIzaSyDuZBtHAhq9RohACFjzcreux7L1JJBnOV0',
-  authDomain: 'selaka-cv.firebaseapp.com',
-  projectId: 'selaka-cv',
-  storageBucket: 'selaka-cv.appspot.com',
-  messagingSenderId: '232715702190',
-  appId: '1:232715702190:web:baded6bb154a36ee70c23f',
-  measurementId: 'G-76Q6V7VZQ5'
+  apiKey: "AIzaSyAxYPcDzn1lSdpD1qDwLTRbNYQB_X8SjeQ",
+  authDomain: "selaka-portfolio.firebaseapp.com",
+  projectId: "selaka-portfolio",
+  storageBucket: "selaka-portfolio.appspot.com",
+  messagingSenderId: "784832517356",
+  appId: "1:784832517356:web:0e4fa5c7372ad1b3c2c1c9",
+  measurementId: "G-DGZ3V06RV7"
 };
 function App() {
   const app = initializeApp(firebaseConfig);
